b17a80b9d5c40ee0714858a17596f0ebde409788
MrHassanNasir24 <hassan@hugoenergyapp.co.uk>
2024-06-29 18:32:37 +0500

    till section 4 video 14

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,44 @@
-import { View, Text } from "react-native";
-const PrimaryButton = ({ children }) => {
+import { View, Text, Pressable, StyleSheet } from "react-native";
+const PrimaryButton = ({ children, onPress }) => {
 	return (
-		<View>
-			<Text>{children}</Text>
+		<View style={styles.buttonOuterContainer}>
+			<Pressable
+				android_ripple={{ color: "#640233" }}
+				style={({ pressed }) =>
+					pressed
+						? [styles.buttonInnerContainer, styles.pressed]
+						: styles.buttonInnerContainer
+				}
+				onPress={onPress}
+			>
+				<Text style={styles.buttonText}>{children}</Text>
+			</Pressable>
 		</View>
 	);
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+	buttonOuterContainer: {
+		borderRadius: 28,
+		margin: 4,
+		overflow: "hidden",
+	},
+	buttonInnerContainer: {
+		backgroundColor: "#72063c",
+		paddingVertical: 8,
+		paddingHorizontal: 16,
+		elevation: 2,
+	},
+	button: {},
+	buttonText: {
+		textAlign: "center",
+		color: "white",
+	},
+
+	// ripple effect for ios
+	pressed: {
+		opacity: 0.75,
+	},
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,86 @@
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
 
 const StartGameScreen = (props) => {
+	const [enteredNumber, setEnteredNumber] = useState("");
+
+	const numberInputHandler = (enteredText) => {
+		setEnteredNumber(enteredText);
+	};
+
+	const resetInputHandler = () => {
+		setEnteredNumber("");
+	};
+
+	const confirmInputHandler = () => {
+		const chosenNumber = parseInt(enteredNumber);
+		if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+			Alert.alert("Invalid number", "please enter a number between 1 and 99.", [
+				{ text: "Okay", style: "destructive", onPress: resetInputHandler },
+			]);
+			return;
+		}
+		console.log(chosenNumber);
+	};
+
 	return (
-		<View>
-			<TextInput />
-			<PrimaryButton>Reset</PrimaryButton>
-			<PrimaryButton>Confirm</PrimaryButton>
+		<View style={styles.inputContainer}>
+			<TextInput
+				style={styles.numberInput}
+				keyboardType="number-pad"
+				maxLength={2}
+				//below props are not needed but still I want to show it can be done
+				// autoCapitalize="none"
+				// autoCorrect={false}
+				onChangeText={numberInputHandler}
+				value={enteredNumber}
+			/>
+			<View style={styles.buttonsContainer}>
+				<View style={styles.buttonContainer}>
+					<PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+				</View>
+				<View style={styles.buttonContainer}>
+					<PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+				</View>
+			</View>
 		</View>
 	);
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+	inputContainer: {
+		justifyContent: "center",
+		alignItems: "center",
+		padding: 16,
+		marginTop: 100,
+		backgroundColor: "#3b021f",
+		marginHorizontal: 24,
+		borderRadius: 8,
+		elevation: 4, //for shadow it will only works on android not on ios
+		// for ios use these 4 properties
+		shadowColor: "black",
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 6,
+		shadowOpacity: 0.25,
+	},
+	numberInput: {
+		height: 50,
+		width: 50,
+		fontSize: 32,
+		fontWeight: "bold",
+		borderBottomColor: "#ddb52f",
+		borderBottomWidth: 2,
+		color: "#ddb52f",
+		marginVertical: 8,
+		textAlign: "center",
+	},
+	buttonsContainer: {
+		flexDirection: "row",
+	},
+	buttonContainer: {
+		flex: 1,
+	},
+});
