98831432ba66e5711b507cd06358535d60d2de92
MrHassanNasir24 <hassan@hugoenergyapp.co.uk>
2024-07-02 23:23:29 +0500

    section 4 till video 25

--- App.js
+++ App.js
@@ -1,18 +1,53 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	View,
+	ImageBackground,
+	SafeAreaView,
+} from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 import { LinearGradient } from "expo-linear-gradient";
+import Colors from "./utils/colors";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
+	const [userNumber, setUserNumber] = useState(null);
+	const [gameIsOver, setGameIsOver] = useState(true);
+
+	function pickedGameHandler(pickedNumber) {
+		setUserNumber(pickedNumber);
+		setGameIsOver(false);
+	}
+
+	function gameOverHandler() {
+		setGameIsOver(true);
+	} 
+	let screen = <StartGameScreen pickNumber={pickedGameHandler} />;
+	if (userNumber) {
+		screen = (
+			<GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+		);
+	}
+
+	if (gameIsOver && userNumber) {
+		screen = <GameOverScreen userNumber={userNumber} />;
+	}
+
 	return (
-		<LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+		<LinearGradient
+			colors={[Colors.primary700, Colors.accent500]}
+			style={styles.rootScreen}
+		>
 			<ImageBackground
 				source={require("./assets/images/background.png")}
 				resizeMode="cover"
 				style={styles.rootScreen}
 				imageStyle={styles.backgroundImage}
 			>
-				<StartGameScreen />
+				<SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
 			</ImageBackground>
 		</LinearGradient>
 	);
