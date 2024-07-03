363c7ff577f5b10af9fc67f60d0fa215f28ba757
MrHassanNasir24 <hassan@hugoenergyapp.co.uk>
2024-07-03 23:14:51 +0500

    section 4 complete start section 5

--- App.js
+++ App.js
@@ -12,18 +12,30 @@ import GameScreen from "./screens/GameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import Colors from "./utils/colors";
 import GameOverScreen from "./screens/GameOverScreen";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
 	const [userNumber, setUserNumber] = useState(null);
 	const [gameIsOver, setGameIsOver] = useState(true);
+	const [guessRounds, setGuessRounds] = useState(0);
 
+	const [fontsLoaded] = useFonts({
+		'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+		'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+	})
+
+	if(!fontsLoaded) {
+		return <AppLoading />
+	}
 	function pickedGameHandler(pickedNumber) {
 		setUserNumber(pickedNumber);
 		setGameIsOver(false);
 	}
 
-	function gameOverHandler() {
+	function gameOverHandler(numberOfRounds) {
 		setGameIsOver(true);
+		setGuessRounds(numberOfRounds);
 	} 
 	let screen = <StartGameScreen pickNumber={pickedGameHandler} />;
 	if (userNumber) {
@@ -32,8 +44,14 @@ export default function App() {
 		);
 	}
 
+	function startNewGameHandler() {
+		setUserNumber(null);
+		setGameIsOver(true);
+		setGuessRounds(0);
+	}
+
 	if (gameIsOver && userNumber) {
-		screen = <GameOverScreen userNumber={userNumber} />;
+		screen = <GameOverScreen userNumber={userNumber} onRestart={startNewGameHandler} roundsNumber={guessRounds} />;
 	}
 
 	return (
